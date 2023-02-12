765a857df5d059dbb0841d302e6f9357af7cba75
Andres Lopez <am.lopezr1@uniandes.edu.co>
2023-02-12 18:47:24 -0500

    add initial configs - healthcheck - pipeline

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
+import { HttpModule } from '@nestjs/axios';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getEnvPath } from './config/env.helper';
+import { HealthModule } from './health/health.module';
+import { TypeOrmConfigService } from './shared/typeorm/typeorm.service';
+
+const envFilePath: string = getEnvPath(`${__dirname}/environments`);
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath,
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({ useClass: TypeOrmConfigService }),
+    HttpModule,
+    HealthModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
